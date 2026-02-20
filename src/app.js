be15802b7928233aeb64d0187cb46b78cc00b329
Alicia2202 <alicialf02@gmail.com>
2026-02-20 19:19:59 +0000

    Generador nombres de dominio

--- src/app.js
+++ src/app.js
@@ -9,3 +9,18 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let dom = ['.com', '.es', '.io'];
+
+pronoun.forEach(i =>{
+  adj.forEach(j =>{
+    noun.forEach(k =>{
+      dom.forEach(l =>{
+        console.log(`${i}${j}${k}${l}`)
+      })
+    })
+  })
+})
